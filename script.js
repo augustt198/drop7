dcfe3598df8f32f50fa372dd0f30de7bf01ac22a
August <augustt198@gmail.com>
2015-07-20 23:10:02 -0400

    Changes

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ var PAPER = Raphael("container")
             .setViewBox(0, 0, 700, 700, true)
             .setSize('100%', '130%');
 
+{
+    var scoreNode = document.createElement("H1");
+    scoreNode.setAttribute("id", "score");
+    var scoreText = document.createTextNode("0");
+    scoreNode.appendChild(scoreText);
+    document.getElementById("container").insertBefore(scoreNode, PAPER.canvas);
+}
+
+var SCORE = 0;
+
 var HARDCORE = true;
 var GRID;
 var BACKGROUND_SLOTS;
@@ -11,6 +21,16 @@ var CURRENT_COLUMN = 3;
 
 
 const DISC_X_INC = 101;
+
+const COLOR_MAP = {
+    1: "#69bd45",
+    2: "#efe609",
+    3: "#e78624",
+    4: "#d82a26",
+    5: "#b8499b",
+    6: "#21bcee",
+    7: "#4556a6"
+};
 
 
 function createGrid() {
@@ -55,22 +75,68 @@ function columnClick(column, index) {
         return;
     }
 
-    dropped.image.animate({x: 101 * index, y: 101 * y}, 400);
-
     GRID[index][y] = dropped.number;
+    IMAGES[index][y] = dropped.image;
+
+    dropped.image.animate({x: 101 * index, y: 101 * y}, 400, "easeIn", function() {
+        drawUpdates(1, function() {
+            addDisc();
+        });
+    });
+}
+
+function drawUpdates(chainLen, finishCallback) {
+    console.log("chainlen = " + chainLen);
+    var update = getDisappearing();
+    if (Object.keys(update[1]).length < 1) {
+        finishCallback();
+        return;
+    }
+
+    var score = chainScore(chainLen);
+
+    chainDelay(eachBgSlot(update, function(slot) {
+        slot.animate({fill: 'white', opacity: 1.0}, 50);
+    }), 300, function() {
+        eachBgSlot(update, function(slot) {
+            slot.animate({fill: 'black', opacity: 0.0}, 50);
+        })();
+        applyGravity();
+        drawUpdates(chainLen + 1, finishCallback);
+    }, 700);
+
+    chainDelay(function() {
+        Object.keys(update[0]).forEach(function(k) {
+            var pair = unpackCoordinate(parseInt(k));
+            var x = pair[0], y = pair[1];
+
+            SCORE += score;
+            document.getElementById("score").textContent = SCORE;
+
+            var text = PAPER.text(x * 101 + 50, y * 101, "+" + score);
+            text.attr({
+                "font-size": 50,
+                "font-family": "Helvetica",
+                "font-weight": 700,
+                "fill": COLOR_MAP[GRID[x][y] + 1]
+            });
+
+            text.animate({y: 101 * y - 150, opacity: 0}, 2500, function() {
+                text.remove();
+            });
+
+            GRID[x][y] = undefined;
+            IMAGES[x][y].animate({transform: 's1.1'}, 100);
+            chainDelay(function() {
+                IMAGES[x][y].animate({transform: 's0', opacity: 0}, 500);
+            }, 180);
+        });
+    }, 300);
 }
 
 function drawBoard() {
     BACKGROUND_SLOTS = new Array(7);
     for (var i = 0; i <= 6; i++) {
-        BACKGROUND_SLOTS[i] = new Array(7);
-        for (var j = 0; j <= 6; j++) {
-            var rect = PAPER.rect(i * 100, j * 100, 100, 100)
-                .attr('fill', '#000');
-
-            BACKGROUND_SLOTS[i][j] = rect;
-        }
-
         col = PAPER.rect(i * 100, 0, 100, 700)
             .attr('fill', '#000')
             .attr('opacity', 0.0);
@@ -87,6 +153,16 @@ function drawBoard() {
                 columnClick(c, idx);
             })
         })(col, i);
+
+        BACKGROUND_SLOTS[i] = new Array(7);
+        for (var j = 0; j <= 6; j++) {
+            var rect = PAPER.rect(i * 100, j * 100, 100, 100)
+                .attr('fill', '#000')
+                .attr('opacity', 0.0);
+            rect[0].setAttribute("class", "square");
+
+            BACKGROUND_SLOTS[i][j] = rect;
+        }
     }
 
     for (var i = 1; i <= 6; i++) {
@@ -118,10 +194,7 @@ function drawDiscs() {
 
 function addDisc() {
     var number  = Math.floor(Math.random() * 7);
-    var xPos    = 3 * DISC_X_INC;
-    if (CURRENT_DISC !== undefined) {
-        xPos = CURRENT_DISC.image.attr('x');
-    }
+    var xPos    = CURRENT_COLUMN * DISC_X_INC;
 
     var imageURL = "svg/disc" + (number + 1) + ".svg";
     var image    = PAPER.image(imageURL, xPos, -100, 95, 95);
@@ -149,11 +222,19 @@ function testDisc1Disappearing(grid, x, y) {
 //
 // sweeps from left to right, top to bottom
 //
-function getDisappearing() {
-    var ret = {};
+// Returns: An array, [discs, ranges]
+// * discs is an object whose keys are
+// packed coordinates obtained by packCoordinate()
+//
+// * ranges is an object whose keys are
+// packed pairs of coordinates obtained by
+// packCoordinatePair()
 
-    var lastHSweepX;
-    var highestHSweepY;
+function getDisappearing() {
+    // hacky sets
+    var discs  = {};
+    var ranges = {};
+
     for (var x = 0; x < GRID.length; x++) {
         var y = 0;
         // get "peak"
@@ -165,116 +246,59 @@ function getDisappearing() {
         for (var y2 = GRID.length - 1; y2 >= y; y2--) {
             var disc = GRID[x][y2];
             if (disc == 0 && testDisc1Disappearing(GRID, x, y2)) {
-                var key = packCoordinate(x, y2, x, y2);
-                ret[key] = [[x, y2]];
+                var range = packCoordinatePair(x, y2, x, y2);
+                ranges[range] = true;
+                var disc = packCoordinate(x, y2);
+                discs[disc] = true;
             } else if (disc == 6 - y) {
-                var key = packCoordinate(x, y, x, GRID[0].length - 1);
-                var v   = [x, y2];
-                ret[key] ? ret[key].push(v) : (ret[key] = [v]);
+                var range = packCoordinatePair(x, y, x, GRID[0].length - 1);
+                ranges[range] = true;
+                var disc = packCoordinate(x, y2);
+                discs[disc] = true;
             }
 
-            if (y2 >= highestHSweepY && lastHSweepX == x - 1) {
-                continue;
-            }
 
-            var lenX = 1;
+            var lenX = 0;
+            var rXbound;
             for (var x2 = x + 1; x2 < GRID.length; x2++) {
                 if (GRID[x2][y2] === undefined) {
+                    rXbound = x2 - 1;
+                    break;
+                } else {
+                    lenX++;
+                }
+            }
+            var lXBound;
+            for (var x2 = x - 1; x2 >= 0; x2--) {
+                if (GRID[x2][y2] === undefined) {
+                    lXBound = x2 + 1;
                     break;
                 } else {
                     lenX++;
                 }
             }
 
-            if (lenX > 1 && lastHSweepX != x) {
-                lastHSweepX = x;
-                highestHSweepY = y2;
+            if (disc != 0 && disc == lenX) {
+                var range = packCoordinatePair(lXBound, y2, rXbound, y2);
+                ranges[range] = true;
+                var disc = packCoordinate(x, y2);
+                discs[disc] = true;
             }
+        }
 
-            for (var x2 = x; x2 < x + lenX; x2++) {
-                if (GRID[x2][y2] != 0 && GRID[x2][y2] == lenX - 1) {
-                    var key = packCoordinate(x, y2, x + lenX - 1, y2);
-                    var v   = [x2, y2];
-                    ret[key] ? ret[key].push(v) : (ret[key] = [v]);
-                }
-            }
-        }
-        if (y <= highestHSweepY && x - 1 == lastHSweepX) {
-            highestHSweepY = y;
-            lastHSweepX++;
-        }
     }
-    return ret;
+    return [discs, ranges];
 }
 
-function findRows(grid) {
-    var any = false;
-    for (var x = 0; x < grid.length; x++) {
-        for (var y = 0; y < grid[x].length; y++) {
-            if (testSlot(grid, x, y)) {
-                any = true;
-                grid[x][y] = undefined;
-            }
-        }
-    }
-    return any;
-}
-
-function printGrid(grid) {
-    for (var y = 0; y < grid[0].length; y++) {
+function printGrid() {
+    for (var y = 0; y < GRID[0].length; y++) {
         var str = "";
-        for (var x = 0; x < grid.length; x++) {
-            var part = grid[x][y] === undefined ? " " : grid[x][y];
+        for (var x = 0; x < GRID.length; x++) {
+            var part = GRID[x][y] === undefined ? " " : GRID[x][y];
             str += part + " ";
         }
         console.log(str);
     }
-}
-
-function testSlot(grid, x, y) {
-    var slot = grid[x][y];
-    if (slot === undefined || slot > 6) {
-        return false;
-    }
-    slot++;
-    if (slot == 1) {
-        var xNeighbors = (x - 1 < 0 || grid[x - 1][y] === undefined) &&
-                         (x + 1 > 6 || grid[x + 1][y] === undefined);
-        var yNeighbors = (y + 1 > 6) && (grid[x][y - 1] === undefined);
-        return xNeighbors || yNeighbors;
-    }
-
-    lenY = 0;
-    // go up
-
-    for (var y2 = 6; y2 >= 0; y2--) {
-        if (grid[x][y2] === undefined) {
-            break;
-        } else {
-            lenY++;
-        }
-    }
-    if (lenY == slot) {
-        return true;
-    }
-
-    lenX = 1;
-    for (var x2 = x + 1; x2 < 7; x2++) {
-        if (grid[x2][y] === undefined) {
-            break;
-        } else {
-            lenX++;
-        }
-    }
-    for (var x2 = x - 1; x2 >= 0; x2--) {
-        if (grid[x2][y] === undefined) {
-            break;
-        } else {
-            lenX++;
-        }
-    }
-
-    return lenX == slot;
 }
 
 function applyGravity() {
@@ -285,52 +309,21 @@ function applyGravity() {
             if (slot === undefined) {
                 offset++;
             } else {
-                GRID[x][y] = undefined;
-                GRID[x][y + offset] = slot;
+                if (offset != 0) {
+                    GRID[x][y] = undefined;
+                    GRID[x][y + offset] = slot;
+                    if (IMAGES !== undefined) {
+                        var img = IMAGES[x][y];
+                        IMAGES[x][y] = undefined;
+                        IMAGES[x][y + offset] = img;
+                        IMAGES[x][y + offset].animate(
+                            {y: 100.5 * (y + offset)}, 170 * offset
+                        );
+                    }
+                }
             }
         }
     }
-}
-
-function populatePaper(grid, paper) {
-    var images = new Array(7);
-    for (var x = 0; x < grid.length; x++) {
-        images[x] = new Array(7);
-        for (var y = 0; y < grid[x].length; y++) {
-            var slot = grid[x][y];
-            if (slot !== undefined) {
-                var url = "svg/disc" + (slot + 1) + ".svg";
-                var image = paper.image(url, 100.8 * x, 100.5 * y, 95, 95);
-                images[x][y] = image;
-            }
-        }
-    }
-    return images;
-}
-
-function columnClicked(idx, hoverInFunc) {
-    var oldX = currentDiscImg.attr('x');
-    var targetY = 0;
-    for (; targetY < grid[0].length; targetY++) {
-        if (grid[idx][targetY] !== undefined) {
-            break;
-        }
-    }
-    targetY--;
-
-    currentDiscImg.animate({y: 100.5 * targetY}, 400, "ease-in", function() {
-        currentDisc = Math.floor(Math.random() * 7);
-        var url = "svg/disc" + (currentDisc + 1) + ".svg"
-        currentDiscImg = paper.image(url, oldX, -100, 95, 95);
-
-        findRows(grid);
-        // applyGravity(grid);
-        hoverInFunc();
-    });
-
-    images[idx][targetY] = currentDiscImg;
-    grid[idx][targetY] = currentDisc;
-    currentDiscImg = null;
 }
 
 /*************
@@ -338,35 +331,73 @@ function columnClicked(idx, hoverInFunc) {
  *************/
 
 // 0 <= all args < 7
-function packCoordinate(x1, y1, x2, y2) {
+function packCoordinatePair(x1, y1, x2, y2) {
     return x1 | (y1 << 3) | (x2 << 6) | (y2 << 9);
 }
 
 // return [x1, y2, x2, y2];
-function unpackCoordinate(packed) {
+function unpackCoordinatePair(packed) {
     return [packed & 7, (packed >> 3) & 7, (packed >> 6) & 7, packed >> 9];
 }
 
+function packCoordinate(x, y) {
+    return x | y << 3;
+}
 
+function unpackCoordinate(packed) {
+    return [packed & 7, (packed >> 3) & 7];
+}
+
+function chainDelay() {
+    var timeOffset = 0;
+    for (var i = 0; i < arguments.length; i += 2) {
+        var callback = arguments[i];
+        var delay    = arguments[i + 1];
+        setTimeout(callback, timeOffset + delay);
+        timeOffset += delay;
+    }
+}
+
+function chainScore(chainLen) {
+    // http://programmablebrick.blogspot.com/2013/03/drop7-with-lego-mindstorms-nxt.html
+    return Math.floor(7 * Math.pow(chainLen, 2.5));
+}
+
+function eachBgSlot(update, callback) {
+    return function() {
+        Object.keys(update[1]).forEach(function(k) {
+            var arr = unpackCoordinatePair(parseInt(k));
+            var x1 = arr[0], y1 = arr[1], x2 = arr[2], y2 = arr[3];
+            console.log(x1, y1, x2, y2);
+            for (var x = x1; x <= x2; x++) {
+                for (var y = y1; y <= y2; y++) {
+                    callback(BACKGROUND_SLOTS[x][y]);
+                }
+            }
+        });
+    };
+}
 
 // driver
 
 function simulateGrid() {
-    var obj = getDisappearing();
+    var discs = getDisappearing()[0];
 
-    while (Object.keys(obj).length > 0) {
-        Object.keys(obj).forEach(function(k) {
-            obj[k].forEach(function(pair) {
-                var x = pair[0], y = pair[1];
-                GRID[x][y] = undefined;
-            });
+
+    while (Object.keys(discs).length > 0) {
+        Object.keys(discs).forEach(function(k) {
+            var pair = unpackCoordinate(parseInt(k));
+            var x = pair[0], y = pair[1];
+            GRID[x][y] = undefined;
         });
 
         applyGravity();
 
-        obj = getDisappearing();
+        discs = getDisappearing()[0];
     }
 }
+
+PAPER.rect(0, 0, 700, 700).attr('fill', 'black');
 
 function game() {
     createGrid();
